26de061aeb5340b6a39bfa343c7f107aa9056e69
Joao Vitor <githubconta2021@gmail.com>
2025-03-02 12:29:02 -0300

    first commit

---
+++ COMANDOS INICIAIS/SELECT/select.sql
@@ -0,0 +1,18 @@
+-- PRECISO DE LISTE O EMAIL DE TODOS OS CLIENTES -- 
+SELECT email
+FROM sales.customers;
+
+-- SELE~]AO DE MAIS DE UMA COLUNA DE UMA TABELA -- 
+SELECT first_name, last_name, email 
+FROM sales.customers;
+
+-- SELECIONAR TODAS AS COLUNAS DE UMA TABELA -- 
+SELECT * FROM sales.customers;
+
+
+
+
+
+
+
+
