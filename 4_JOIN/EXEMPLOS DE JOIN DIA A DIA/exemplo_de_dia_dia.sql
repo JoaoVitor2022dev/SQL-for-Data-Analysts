75b8f04a772debdab401072a535ad03e1d27ed58
Joao Vitor <githubconta2021@gmail.com>
2025-03-24 10:06:21 -0300

    Description: EXEMPLO JOIN

--- 4_JOIN/EXEMPLOS DE JOIN DIA A DIA/exemplo_de_dia_dia.sql
+++ 4_JOIN/EXEMPLOS DE JOIN DIA A DIA/exemplo_de_dia_dia.sql
@@ -38,5 +38,62 @@ LEFT JOIN temp_tables.ibge_genders AS ibge
    ON LOWER(cus.first_name) = ibge.first_name
    
 GROUP BY ibge.gender; 
-
 -- 
+
+-- (Exemplo 3) Identifique de quais regiões são os clientes que mais visitam o site
+-- Obs: Utilizar a tabela temp_tables.regions
+
+select * from sales.customers limit 10;
+select * from temp_tables.regions limit 10;
+
+SELECT * FROM sales.funnel;
+
+
+SELECT 
+     Regiao.region,
+     COUNT(fun.visit_page_date) AS Visitas
+     
+-- Selecionando os campos que deseja extrair e a contagem dos registros de cada tabela de clientes.
+    
+FROM sales.funnel AS fun
+LEFT JOIN sales.customers AS Cliente
+    ON fun.customer_id = Cliente.customer_id
+       
+-- Conexão entre duas tabelas por meio do JOIN. 
+
+LEFT JOIN temp_tables.regions AS Regiao
+    ON LOWER(Cliente.city) = LOWER(Regiao.city)
+    AND LOWER(Cliente.state) = LOWER(Regiao.state)
+    
+-- Conexão entre os registros de cidade e estado, com a verificação de que a cidade e o estado são os mesmos. 
+
+GROUP BY Regiao.region
+ORDER BY Visitas DESC; 
+
+-- Agrupamento geral baseado no Regiao.region. Quando se usa a função COUNT, é necessário aplicar o agrupamento,
+-- caso contrário, ocorrerá um erro na consulta, pois a query não saberá como organizar os dados. 
+
+
+	   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
