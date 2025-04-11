2ff7fd9361e77a8980411d888d188c3bbf8e0571
Joao Vitor <githubconta2021@gmail.com>
2025-04-11 09:58:06 -0300

    Description: TRATAMENTO DE DATAS

--- 7_TRATAMENTO_DE_DADOS/TRATAMENTO_DATA/tratamento_data.sql
+++ 7_TRATAMENTO_DE_DADOS/TRATAMENTO_DATA/tratamento_data.sql
@@ -51,10 +51,34 @@ SELECT
 	EXTRACT('dow'FROM '2022-01-30'::DATE)
 
 
+-- (Exemplo 3) Extração de unidades de uma data utilizando EXTRACT
+-- Calcule qual é o dia da semana que mais recebe visitas ao site
 
 
+SELECT 
+   EXTRACT('dow' FROM visit_page_date) AS dia_da_semana, 
+   COUNT(*)
+FROM sales.funnel
+GROUP BY dia_da_semana; 
+
+-- (Exemplo 4) Diferença entre datas com operador de subtração (-) 
+-- Calcule a diferença entre hoje e '2018-06-01', em dias, semanas, meses e anos.
+
+SELECT (CURRENT_DATE - '2018-06-01') 
+SELECT (CURRENT_DATE - '2018-06-01')/ 7 -- dia 
+SELECT (CURRENT_DATE - '2018-06-01')/ 30 -- mes
+SELECT (CURRENT_DATE - '2018-06-01')/ 365 -- ano 
+
+SELECT DATEDIFF('weeks', '2018-06-01', CURRENT_DATE)
 
 
-
-
-
+-- RESUMO --------------------------------------------------------------------------
+-- (1) O comando INTERVAL é utilizado para somar datas na unidade desejada. Caso a 
+-- unidade não seja informada, o SQL irá entender que a soma foi feita em dias.
+-- (2) O comando DATE_TRUNC é utilizado para truncar uma data no início do período
+-- (3) O comando EXTRACT é utilizado para extrair unidades de uma data/timestamp
+-- (4) O cálculo da diferença entre datas com o operador de subtração (-) retorna  
+-- valores em dias. Para calcular a diferença entre datas em outra unidade é necessário
+-- fazer uma transformação de unidades (ou criar uma função para isso)
+-- (5) Utilize o Guia de comandos para consultar as unidades de data e hora utilizadas 
+-- no SQL
